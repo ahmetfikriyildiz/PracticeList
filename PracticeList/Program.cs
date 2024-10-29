49058818ac403ae4e6adbda94a47bb416897d57c
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-29 14:20:13 +0300

    Add project files.

---
+++ PracticeList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            while (true) 
+            {
+                Console.WriteLine("Lütfen davet etmek istediğiniz kişilerin isimlerini giriniz(İşleminiz bittiğinde 'q' yazınız.) : ");
+                string invite = Console.ReadLine();
+
+                if ( invite == "q")
+                {
+                    break;
+                }
+
+                list.Add(invite);
+            }
+
+            Console.WriteLine("\nDavetliletin listesi :");
+            foreach (string guests in list)
+            {
+                Console.WriteLine(guests);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticeList/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PracticeList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PracticeList")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be83b8f7-150d-40bc-b5a4-dc716336f4b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
